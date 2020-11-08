ee87389afb9a2a4dbadf6e07c3b4ed12059813e1
mrjared16 <doannguyentanphuc99@gmail.com>
2020-11-08 19:08:19 +0700

    add auth service

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,31 @@
+import { LocalAuthenticationGuard } from './local/local.guard';
+import { Body, HttpCode, Req } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { Controller, Request, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { RegisterDto, RequestWithUser } from './auth.dto';
+
+@Controller('auth')
+export class AuthController {
+    constructor(
+        private readonly authService: AuthService
+    ) { }
+
+    // @HttpCode(200)
+    @UseGuards(LocalAuthenticationGuard)
+    @Post('login')
+    async logIn(@Req() request: RequestWithUser) {
+        // console.log({ request });
+        const user = request.user;
+        if (user == null) {
+            return;
+        }
+        const accessToken = this.authService.getToken(user);
+        return accessToken;
+    }
+
+    @Post('register')
+    async register(@Body() registrationData: RegisterDto) {
+        return this.authService.register(registrationData);
+    }
+}
