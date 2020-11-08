8e06ce5457858db3f97d0a7f9ea0b4f1a79bec1b
mrjared16 <doannguyentanphuc99@gmail.com>
2020-11-08 22:52:54 +0700

    protect with access control

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,14 +1,16 @@
-import { JWTAuthenticationGuard } from '../auth/jwt/jwt.guard';
-import { Body, Controller, Param, Patch, Post, UseGuards } from "@nestjs/common";
+import { DACAuthenticationGuard } from './../auth/guards/dac.guard';
+import { JWTAuthenticationGuard } from '../auth/guards/jwt.guard';
+import { Body, Controller, forwardRef, Inject, Param, Patch, Post, Req, UseGuards } from "@nestjs/common";
 import { UpdateUserDto, UserDto } from "./users.dto";
 import { UserService } from "./users.service";
+import { RequestWithToken } from 'src/auth/auth.dto';
 
 
 @Controller('users')
 export class UserController {
-    constructor(private readonly userService: UserService) {
-
-    }
+    constructor(
+        private readonly userService: UserService
+    ) { }
 
     @Post()
     createUser(@Body() user: UserDto) {
@@ -16,13 +18,13 @@ export class UserController {
     }
 
     @Patch(':id')
-    @UseGuards(JWTAuthenticationGuard)
+    @UseGuards(JWTAuthenticationGuard, DACAuthenticationGuard)
     updateUser(
         @Param('id') id: string,
-        @Body() user: UpdateUserDto
+        @Body() userData: UpdateUserDto,
+        @Req() request: RequestWithToken
     ) {
-        const { ...userData } = user;
-        // console.log({ user, id });
-        return this.userService.updateUser(id, userData);
+        const { name } = userData;
+        return this.userService.updateUser(id, { name });
     }
 }
