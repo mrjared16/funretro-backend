1021a7557f8be88820b9a057eb79bd9b86c017a8
mrjared16 <doannguyentanphuc99@gmail.com>
2020-11-13 01:31:37 +0700

    add card repository decorator

--- src/cards/cards.service.ts
+++ src/cards/cards.service.ts
@@ -1,9 +1,12 @@
 import { Repository } from 'typeorm';
 import { Injectable } from '@nestjs/common';
 import { CardEntity } from './cards.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class CardService {
-    constructor(private cardRepository: Repository<CardEntity>) { }
+    constructor(
+        @InjectRepository(CardEntity)
+        private cardRepository: Repository<CardEntity>) { }
 
 }
