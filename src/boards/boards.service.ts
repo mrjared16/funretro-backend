4e5107cb6c8f86e1ba0c4693e7340e4bf37dbd9d
mrjared16 <doannguyentanphuc99@gmail.com>
2020-11-13 01:29:18 +0700

    add create board service

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,6 +1,24 @@
+import { UserEntity } from 'src/users/users.entity';
+import { Repository } from 'typeorm';
 import { Injectable } from '@nestjs/common';
+import { BoardData, BoardDTO } from './boards.dto';
+import { BoardEntity } from './boards.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class BoardService {
-    
+    constructor(
+        @InjectRepository(BoardEntity)
+        private readonly boardRepository: Repository<BoardEntity>
+    ) {
+
+    }
+
+    async createBoard(boardData: BoardData, idUser: string): Promise<BoardDTO> {
+        const { name } = boardData;
+        const user = { id: idUser };
+        const newBoard = await this.boardRepository.create({ name, user: user as UserEntity });
+        await this.boardRepository.save(newBoard);
+        return BoardDTO.EntityToDTO(newBoard);
+    }
 }
