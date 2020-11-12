06b0bf5dcad074df298ba22e1e9ef9572ee1d7c7
mrjared16 <doannguyentanphuc99@gmail.com>
2020-11-13 01:30:47 +0700

    add create list and lists with template services

--- src/lists/lists.service.ts
+++ src/lists/lists.service.ts
@@ -1,19 +1,55 @@
+import { BoardEntity } from 'src/boards/boards.entity';
+import { ListData, ListDTO } from 'src/lists/lists.dto';
+import { CreateListDTO } from './lists.dto';
 import { Repository } from 'typeorm';
 import { Injectable } from '@nestjs/common';
 import { ListEntity } from './lists.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 
 @Injectable()
 export class ListService {
-    constructor(private readonly listRepository: Repository<ListEntity>) {
+    constructor(
+        @InjectRepository(ListEntity)
+        private readonly listRepository: Repository<ListEntity>) {
 
     }
 
-    async createList(boardID: string) {
+    async createList(data: CreateListDTO): Promise<ListDTO> {
+        const { name, color = '', pos, idBoard } = data;
+        const board = { id: idBoard };
 
+        const newList = this.listRepository.create({ name, color, pos, board: board as BoardEntity });
+        this.listRepository.save(newList);
+
+        return ListDTO.EntityToDTO(newList);
     }
 
-    async createListsFromTemplate() {
-        
+    getListTemplateData(idBoardTemplate: string): ListData[] {
+        const KanbanList: { name: string, color: string }[] = [{
+            name: 'To do',
+            color: '111'
+        }, {
+            name: 'Doing',
+            color: '222'
+        }, {
+            name: 'Done',
+            color: '333'
+        }];
+
+        const BUFFER = 65355;
+        const defaultBoard = KanbanList.map((item, index) => ({
+            ...item,
+            pos: (index + 1) * BUFFER
+        }));
+        return defaultBoard;
+    }
+
+    async createListsFromTemplate(idBoardTemplate: string, idBoard: string): Promise<ListDTO[]> {
+        const listData = this.getListTemplateData(idBoardTemplate);
+        const response = listData.map(data => this.createList({
+            ...data, idBoard
+        }));
+        return Promise.all(response);
     }
 }
